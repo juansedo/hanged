397329ff76dbcf886b23bead6bbccf3ab321adf0
juansedo <jsdiazo@eafit.edu.co>
2020-10-11 23:17:18 -0500

    basic login animation

--- public/js/animate.js
+++ public/js/animate.js
@@ -2,3 +2,10 @@
 particlesJS.load('particles-js', 'js/particles.json', function() {
     console.log('callback - particles.js config loaded');
 });
+
+anime({
+    targets: '.login-box .title>img',
+    easing: 'easeOutExpo', 
+    scale: [0,1],
+    duration:1000
+  });
